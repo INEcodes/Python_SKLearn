16250116933d2e715cd4a72a099322d6cc05549c
INE Codes <abhayaps05@gmail.com>
2024-01-18 10:44:57 +0530

    plot sns

--- Iris_Flower_kaggle/Bar_plot_sns.py
+++ Iris_Flower_kaggle/Bar_plot_sns.py
@@ -2,7 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 df =pd.read_csv("C:/Users/ADMIN/Documents/GitHub/Python-SKLearn/Iris_Flower_kaggle/IRIS.csv") 
-ax=plt.subplot(1,1,figsize=(10,8))
-sns.countplot('Species',data=df)
+fig, ax = plt.subplots(figsize=(10, 8))
+sns.countplot(x='species',data=df,ax =ax)
 plt.title("Iris species count")
 plt.show()
