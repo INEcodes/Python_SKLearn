367dbd870a3b9d5d759b2558a48738d74bfb30bd
INE Codes <abhayaps05@gmail.com>
2024-01-21 11:30:43 +0530

    more joint plot

--- Iris_Flower_kaggle/individual_distribution.py
+++ Iris_Flower_kaggle/individual_distribution.py
@@ -2,4 +2,8 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 df =pd.read_csv("C:/Users/ADMIN/Documents/GitHub/Python-SKLearn/Iris_Flower_kaggle/IRIS.csv") 
-g = sns.jointplot
+g = sns.jointplot(x='sepal_length', y='sepal_width', data=df, kind="kde",color="m")
+g.plot_joint(plt.scatter,c="b",s=40, linewidth=1, marker="+")
+g.ax_joint.collections[0].set_alpha(0)
+g.set_axis_labels("$SepalLenght$", "$SepalWidth$")
+plt.show()
