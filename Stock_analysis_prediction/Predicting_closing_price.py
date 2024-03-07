9dd6ad0e5ad4307e05f8fd3b3424acae98d3e06d
INE Codes <abhayaps05@gmail.com>
2024-03-08 01:17:41 +0530

    Keras model application

--- Stock_analysis_prediction/Predicting_closing_price.py
+++ Stock_analysis_prediction/Predicting_closing_price.py
@@ -87,3 +87,55 @@ x_train, y_train = np.array(x_train), np.array(y_train)
 x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
 #x_train.shape
 #for ML models we will be using Keras as it is a programmer friendly for the beginners in Deep Neural Networks(DNNs).
+
+from keras.models import Sequential
+from keras.layers import Dense, LSTM
+
+model = Sequential()
+model.add(LSTM(128, return_sequences = True, input_shape = (x_train.shape[1], 1)))
+model.add(LSTM(64, return_sequences=False))
+model.add(Dense(25))
+model.add(Dense(1))
+
+model.compile(optimizer='adam', loss='mean_squared_error')
+
+model.fit(x_train, y_train, batch_size=1, epochs=1)
+
+
+#Create the test dataset:
+#Creating a new array containing scaled values from index 1543 to 2002
+
+test_data = scaled_data[training_data_len - 60:, :]
+
+x_test = []
+
+y_test = dataset[training_data_len:,:]
+for i in range(60, len(test_data)):
+    x_test.append(test_data[i-60:i, 0])
+    
+x_test = np.array(x_test)
+
+x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], 1))
+
+# To get prediction from the ML models
+predictions = model.predict(x_test)
+predictions = scaler.inverse.transform(predictions)
+
+#get the root mean squared error
+rmse = np.sqrt(np.mean(((predictions - y_test) ** 2)))
+print(rmse)
+
+train = data[:training_data_len]
+valid = data[training_data_len:]
+valid['Predictions'] = predictions
+#visualize the data
+plt.figure(figsize=(16,6))
+plt.title('Model')
+plt.xlabel('Date', fontsize=18)
+plt.ylabel('Close Price USD($)', fontsize=18)
+plt.plot(train['Close'])
+plt.plot(valid[['Close', 'Predictions']])
+plt.legend(['Train', 'Val','Predictions'], loc='lower right')
+plt.show()
+
+print(valid)
